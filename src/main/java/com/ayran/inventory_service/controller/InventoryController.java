12048e736c5af24a01829e6123ecaff84392d13f
umut <h.umutayran@gmail.com>
2025-03-09 17:48:47 +0300

    inventory service initial commit

---
+++ src/main/java/com/ayran/inventory_service/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.ayran.inventory_service.controller;
+
+import com.ayran.inventory_service.dto.InventoryResponse;
+import com.ayran.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+
+}
